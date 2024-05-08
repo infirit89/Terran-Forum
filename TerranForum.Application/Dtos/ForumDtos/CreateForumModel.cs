6cf7b0964dbb8d438f21af42d649bbed5c7b9a6e
infirit89 <georgedautov808@gmail.com>
2024-05-08 10:35:33 +0300

    added forum dtos

---
+++ TerranForum.Application/Dtos/ForumDtos/CreateForumModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TerranForum.Application.Dtos.ForumDtos
+{
+    public class CreateForumModel
+    {
+        [Required, MinLength(5)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        public string UserId { get; set; } = null!;
+    }
+}
