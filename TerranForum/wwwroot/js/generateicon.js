46da6d28cc1e02c975cc3cdf95815c3c7898963a
infirit89 <georgedautov808@gmail.com>
2024-06-06 23:45:20 +0300

    added user icons

---
+++ TerranForum/wwwroot/js/generateicon.js
@@ -0,0 +1,18 @@
+﻿import { minidenticon } from 'https://cdn.jsdelivr.net/npm/minidenticons@4.2.1/minidenticons.min.js';
+
+(function setupIcon() {
+	//const manageItem = document.querySelector('#manage-li');
+
+	//if (manageItem === null)
+	//	return;
+
+	//const manageAnchor = manageItem.querySelector('#manage');
+	
+	//manageItem.removeChild(manageAnchor);
+
+	//manageItem.insertAdjacentHTML('afterbegin', minidenticon("laurent", 95, 45));
+	//const userIcon = manageItem.querySelector('svg');
+	//const shapeDefs = [...userIcon.childNodes];
+	//manageAnchor.replaceChildren(...shapeDefs);
+	//userIcon.appendChild(manageAnchor);
+})();
